c730fe15536adc3c0cad7c764b3b012a34c2a130
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-09-25 07:53:46 +0530

    add more walls

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 10f; // Speed of the ball
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Get the Rigidbody attached to the ball
+    }
+
+    void FixedUpdate()
+    {
+        // Get input from the player (keyboard or controller)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a vector for movement
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Apply the movement to the ball using physics
+        rb.AddForce(movement * speed);
+    }
+}
